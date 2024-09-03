b9270e64485cf118b671ab05147a221368f39874
dedybayu <dedybayusetiawan5@gmail.com>
2024-09-03 19:38:39 +0700

    First Commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+/**
+ * Main
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("ayam");
+        ayam();
+    }
+
+    public static void ayam(){
+        System.out.println("babi");
+    }
+    
+}
